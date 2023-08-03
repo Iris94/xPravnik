838e361b28f6ab52726ee84e2347330bbd78a636
Iris94 <mirza.iris94@gmail.com>
2023-08-03 18:56:50 +0200

    Commitment to fix Vercel issues

--- components/Tutorial.tsx
+++ components/Tutorial.tsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import Image from 'next/image'
 
 const Tutorial = () => {
   return (
@@ -8,10 +9,20 @@ const Tutorial = () => {
     </div>
     <div className='w-full h-4/5 flex flex-col lg:flex-row justify-center items-center'>
         <div className='w-full h-full flex justify-center items-center p-5'>
-          <img src='/tutor.png' alt='tutorial' className='object-contain'/>
+        <div className='relative' style={{ width: '80vmin', height: '50vmin' }}>
+          <Image
+            src='/tutor.png'
+            alt='tutorial'
+            layout='fill'
+            objectFit='contain'
+            objectPosition='center'
+          />
+          </div>
         </div>
         <div className='w-full h-full flex items-center justify-center'>
-          <p className='w-2/3 text-base lg:text-lg xl:text-xl 2xl:text-2xl 3xl:text-3xl'>U polju iznad dugmića <span className='font-bold'>unesi zahtjev</span> možete da napišete naredbu za tekst. Na primjer ako vam treba žalba, napišete: <span className='italic'>'Napiši mi žalbu povodom X događaja, jer smatram da se dogodilo to i to'</span>. Nakon što izdate zahtjev, kliknite dugme i u polju pored pojavit će se automatski generisan tekst koji prepravite za sitnice. Nakon što imate osnovni tekst samo trebate popuniti ostale informacije, kliknuti na <span className='font-bold'>unesi informacije</span> i skinuti dokument u PDF formatu</p>
+        <p className='w-2/3 text-base lg:text-lg xl:text-xl 2xl:text-2xl 3xl:text-3xl'>
+          U polju iznad dugmića <span className='font-bold'>unesi zahtjev</span> možete da napišete naredbu za tekst. Na primjer ako vam treba žalba, napišete: <span className='italic'>&lsquo;Napiši mi žalbu povodom X događaja, jer smatram da se dogodilo to i to&rsquo;</span>. Nakon što izdate zahtjev, kliknite dugme i u polju pored pojavit će se automatski generisan tekst koji prepravite za sitnice. Nakon što imate osnovni tekst samo trebate popuniti ostale informacije, kliknuti na <span className='font-bold'>unesi informacije</span> i skinuti dokument u PDF formatu.
+          </p>
         </div>
     </div>
     </div>
