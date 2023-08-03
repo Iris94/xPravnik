ba1d98b670c5c2413e518076d5e73187b99f168d
Iris94 <mirza.iris94@gmail.com>
2023-08-03 18:15:29 +0200

    Made a small edit

--- components/Tutorial.tsx
+++ components/Tutorial.tsx
@@ -11,7 +11,7 @@ const Tutorial = () => {
           <img src='/tutor.png' alt='tutorial' className='object-contain'/>
         </div>
         <div className='w-full h-full flex items-center justify-center'>
-          <p className='w-2/3 text-base lg:text-lg xl:text-xl 2xl:text-2xl 3xl:text-3xl'>U polju iznad dugmića <span className='font-bold'>unesi zahtjev</span> možete da napišete naredbu za tekst. Na primjer ako vam treba žalba, napišete: <span className='italic'>'Napiši mi žalbu povodom X događaja, jer smatram da se dogodilo to i to'</span>. Nakon što izdate zahtjev, kliknite dugme i u polju pored pojavit će se automatski generisan tekst koji prepravite za sitnice. Nakon što imate osnovni tekst samo trebate popuniti ostale informacije, kliknuti na <span className='font-bold'>unesi informacije</span> i skinuti dokument u PDF-u</p>
+          <p className='w-2/3 text-base lg:text-lg xl:text-xl 2xl:text-2xl 3xl:text-3xl'>U polju iznad dugmića <span className='font-bold'>unesi zahtjev</span> možete da napišete naredbu za tekst. Na primjer ako vam treba žalba, napišete: <span className='italic'>'Napiši mi žalbu povodom X događaja, jer smatram da se dogodilo to i to'</span>. Nakon što izdate zahtjev, kliknite dugme i u polju pored pojavit će se automatski generisan tekst koji prepravite za sitnice. Nakon što imate osnovni tekst samo trebate popuniti ostale informacije, kliknuti na <span className='font-bold'>unesi informacije</span> i skinuti dokument u PDF formatu</p>
         </div>
     </div>
     </div>
